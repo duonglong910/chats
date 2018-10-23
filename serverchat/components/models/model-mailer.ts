23a86cd759310ab1ded9ea28aacb8e21b58fffab
Duong Long <duonglong910@gmail.com>
2018-10-23 23:58:32 +0700

    + add file

---
+++ serverchat/components/models/model-mailer.ts
@@ -0,0 +1,23 @@
+﻿var path = require('path');
+module JMath {
+    export class JMailerModel {
+        /**
+        * addMailSend
+        * @param params
+        * @param cb
+        */
+        public addMailSend(params, cb?: { (err, docs: any[]): void }): void {
+            global.logger.trace('JMailerModel.addMailSend...');
+            global.db.callFunc('addMailSend',
+                [params],
+                (error, result) => {
+                    //global.logger.info("getItemCustomers--->", result);
+                    if (error) global.logger.error('JMailerModel.addMailSend->', error);
+                    if (typeof cb === "function")
+                        cb(error, result);
+                });
+        }
+
+    }
+}
+module.exports = new JMath.JMailerModel();
